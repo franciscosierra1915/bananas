ec13e01e43592045c7bdf12205ae805a6924e94b
Francisco Sierra <64598498+franciscosierra1915@users.noreply.github.com>
2022-04-06 10:35:00 -0500

    click event added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import { useRef } from 'react';
-import {Canvas, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
-function Box(){
+function Box() {
   const ref = useRef();
+
+  const [clicked, setClicked] = useState(false);
   useFrame((state) => {
-    ref.current.rotation.x = Math.sin(state.clock.elapsedTime);
+    ref.current.position.z = THREE.MathUtils.lerp(
+      ref.current.position.z,
+      clicked ? 1 : 0,
+      0.1
+    );
   });
 
   return (
-    <mesh ref={ref}>
-      <boxGeometry/>
+    <mesh ref={ref} onClick={() => setClicked(!clicked)}>
+      <boxGeometry />
       <meshBasicMaterial color="orange" />
     </mesh>
-  )
+  );
 }
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <Canvas>
-      <Box/>
+      <Box />
     </Canvas>
-  )
+  );
 }

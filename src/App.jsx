bd4819f2ee5acf90d3fa0c58bd5aff5e0cd1edec
Francisco Sierra <64598498+franciscosierra1915@users.noreply.github.com>
2022-04-07 15:16:46 -0500

    apples rotating

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,52 @@
 import * as THREE from "three";
 import { Suspense, useRef, useState } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { useGLTF, Environment  } from "@react-three/drei"
+import { useGLTF, Environment } from "@react-three/drei";
 
-function Box({ z }) {
+function Apple({ z }) {
   const ref = useRef();
+  const { nodes, materials } = useGLTF("/apple-transformed.glb");
   const { viewport, camera } = useThree();
-  const { width, height } = viewport.getCurrentViewport(camera, [0,0, z])
+  const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
   const [data] = useState({
     x: THREE.MathUtils.randFloatSpread(2),
     y: THREE.MathUtils.randFloatSpread(height),
+    rX: Math.random() * Math.PI,
+    rY: Math.random() * Math.PI,
+    rZ: Math.random() * Math.PI,
   });
 
-
   useFrame((state) => {
-    ref.current.position.set(data.x * width, (data.y += 0.5), z);
+    ref.current.rotation.set((data.rX += 0.001), (data.rY += 0.001), (data.rZ += 0.001))
+    ref.current.position.set(data.x * width, (data.y += 0.01), z);
     if (data.y > height / 1.5) {
       data.y = -height / 1.5;
     }
   });
 
   return (
-    <mesh ref={ref}>
-      <boxGeometry />
-      <meshBasicMaterial color="orange" />
-    </mesh>
+    <mesh
+    ref={ref}
+      geometry={nodes["apple_low_obj_Material_#35_0"].geometry}
+      material={materials.Material_35}
+      position={[0, 0.23, 0.18]}
+      scale={0.05}
+    />
   );
-}
-
-function Apple(){
-  const { scene } = useGLTF("/apple.glb")
-  return <primitive object={scene} />
 }
 
 export default function App({ count = 100 }) {
   return (
     <Canvas>
+      <ambientLight intensity={0.2} />
+      <spotLight position={[10, 10, 10]} intensity={2} />
       <Suspense fallback={null}>
-        <Apple scale={0.5}/>
+      {Array.from({ length: count }, (_, i) => (
+        <Apple key={i} z={-i} />
+      ))}
         <Environment preset="sunset" />
       </Suspense>
-      {/* {Array.from({ length: count }, (_, i) => (
-        <Box key={i} z={-i} />
-      ))} */}
+
     </Canvas>
   );
 }
